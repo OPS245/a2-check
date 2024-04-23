b1533c9b3952381dce3f5bbdac1a23e5a6c46511
Jason Carman <jasonmcarman@gmail.com>
2024-04-23 13:44:12 -0400

    Replaced with check script updated for Ubuntu.

--- marka2.bash
+++ marka2.bash
@@ -1,20 +1,35 @@
 #!/bin/bash
 # Author: Jason Carman; jason.carman@senecapolytechnic.ca
 # Date: March 14, 2023
-# Updated: Februrary 9, 2024
+# Updated: Februrary 22, 2024
 # Purpose: Generate a text file submission for Assignment 2
-# Usage: Run this on deb3, then copy the output file to your host.
+# Usage: Run this on the Ubuntu VM you created in your Assignment 1, then copy the output file to your host.
+#
+
+clear
 
 if [[ $(whoami) != "root" ]]; then
   echo "You must be root to run this script. Please use sudo." 2> /dev/null
   exit 1
 fi
 
-read -p "Please enter your userID: " user
+read -p "Please enter your username: " user
 
 cat << EOF > /home/$user/a2output.txt
 Username: $user
 Unique ID: $(echo $user | sha256sum)
+
+#####
+# Current default target:
+$(systemctl get-default)
+
+#####
+# IP Address:
+$(ip a)
+
+#####
+# nftables configuration:
+$(sudo nft list ruleset)
 
 #####
 # Apache status:
@@ -26,16 +41,11 @@ $(systemctl status mariadb)
 
 #####
 # Wordpress installed:
-$(ls -la /var/www/html/wordpress)
+$(ls -la /usr/share/wordpress)
 
 #####
 # Wordpress config:
-$(head -30 /var/www/html/wordpress/wp-config.php)
-
-#####
-# Firewall configuration:
-$(iptables -L -vn --line-numbers)
-
+$(head -10 /etc/wordpress/config-$user-ubuntu.php)
 EOF
 
 echo "a2output.txt has been created in your home directory.  Please submit it to the Assignment 2 folder on BlackBoard."
